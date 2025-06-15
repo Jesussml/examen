83dd9335f1f47b29b5a1b62a587d9ed01790b37a
unknown <raizor3884@gmail.com>
2025-06-15 17:00:31 -0400

    correccion final

--- src/componentes/Cartilla.jsx
+++ src/componentes/Cartilla.jsx
@@ -1,16 +1,15 @@
 import '../estilos/cartilla.css'
 
-export function Cartilla( {nombre, ruta, desc, enlace} ) {
-    const srcImage = `examen/images/actor_${ruta}`
+export function Cartilla( {nombre, ruta, children, enlace} ) {
 
     return(
         <div className='cartilla'>
             <div className="cartillaHead">
-                <img src={srcImage} />
+                <img src={ruta} />
                 <h3>{nombre}</h3>
             </div>
             <div className="cartillaContenido">
-                <p>{desc}</p>
+                <p>{children}</p>
                 <a href={enlace} target='_blank' ><button className='enlaceBtn'>Leer mas</button></a>
             </div>
         </div>
--- src/componentes/Menu.jsx
+++ src/componentes/Menu.jsx
@@ -4,7 +4,7 @@ import '../estilos/menu.css'
 export function Menu() {
     return(
         <nav>
-            <img className='logo' src='examen/images/logo.png' alt="logo" />
+            <img className='logo' src='https://img.icons8.com/?size=100&id=H2sN5AIZbe5o&format=png&color=EAEFEF' alt="logo" />
             <ul className='links'>
                 <li><Link className='enlace' to='/examen'>Principal</Link></li>
                 <li><Link className='enlace' to='/examen/peliculas'>Peliculas</Link></li>
