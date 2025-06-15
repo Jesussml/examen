122aaf4093165773d4c297e18f0f1cf36dd5f43c
unknown <raizor3884@gmail.com>
2025-06-15 17:10:06 -0400

    agregacion de repositorio

--- src/componentes/Menu.jsx
+++ src/componentes/Menu.jsx
@@ -4,7 +4,9 @@ import '../estilos/menu.css'
 export function Menu() {
     return(
         <nav>
-            <img className='logo' src='https://img.icons8.com/?size=100&id=H2sN5AIZbe5o&format=png&color=EAEFEF' alt="logo" />
+            <a href="https://github.com/Jesussml/examen" target="_blank" >
+                <img className='logo' src='https://img.icons8.com/?size=100&id=H2sN5AIZbe5o&format=png&color=EAEFEF' alt="logo" />
+            </a>
             <ul className='links'>
                 <li><Link className='enlace' to='/examen'>Principal</Link></li>
                 <li><Link className='enlace' to='/examen/peliculas'>Peliculas</Link></li>
